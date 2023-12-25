de70dfd9a3304ab45637947f91868ebdc0255299
Longfk <145273974+Longfk@users.noreply.github.com>
2023-12-25 13:52:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,157 @@
 import cv2
 import os
 from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog
+def select_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4")])
+    if file_path:
+        cap = cv2.VideoCapture(file_path)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (640, 480))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (25, 25), 0)
+        last_frame = gray
+        # Chọn vùng cần quan sát
+        roi = cv2.selectROI('Video', frame, fromCenter=False, showCrosshair=True)
+        cv2.destroyWindow('Video')
+        x, y, w, h = int(roi[0]), int(roi[1]), int(roi[2]), int(roi[3])
 
-ret, frame = cap.read()
-frame = cv2.resize(frame, (640, 480))
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (25, 25), 0)
-last_frame = gray
-frame = cv2.flip(frame, 1)
-# Chọn vùng cần quan sát
-roi = cv2.selectROI('Video', frame, fromCenter=False, showCrosshair=True)
-cv2.destroyWindow('Video')
-x, y, w, h = int(roi[0]), int(roi[1]), int(roi[2]), int(roi[3])
+        print(x, y, w, h)
 
-print(x, y, w, h)
+        # Tạo thư mục chứa ảnh được chụp
+        output_dir = "captured_images"
+        os.makedirs(output_dir, exist_ok=True)
 
-# Tạo thư mục chứa ảnh được chụp
-output_dir = "captured_images"
-os.makedirs(output_dir, exist_ok=True)
+        # Biến thời gian để theo dõi thời gian đã trôi qua từ lần chụp trước đó
+        last_capture_time = datetime.now()
 
-# Biến thời gian để theo dõi thời gian đã trôi qua từ lần chụp trước đó
-last_capture_time = datetime.now()
+        while True:
+            if cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:  # Kiểm tra nếu không có khung hình trả về
+                    print("Video kết thúc")
+                    break
+                frame = cv2.resize(frame, (640, 480))
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-while True:
-    _, frame = cap.read()
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.GaussianBlur(gray, (25, 25), 0)
+
+                abs_img = cv2.absdiff(last_frame[y:y + h, x:x + w], gray[y:y + h, x:x + w])
+                last_frame = gray
+
+
+                _, img_mask = cv2.threshold(abs_img, 2, 255, cv2.THRESH_BINARY)
+                contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                for contour in contours:
+                    if cv2.contourArea(contour) < 900:
+                        continue
+
+                    x1, y1, w1, h1 = cv2.boundingRect(contour)
+                    cv2.rectangle(frame, (x1 + x, y1 + y), (x1 + x + w1, y1 + y + h1), (0, 255, 0), 2)
+                    cv2.putText(frame, 'Canh Bao', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+                    # Kiểm tra thời gian từ lần chụp trước đó
+                    current_time = datetime.now()
+                    time_diff = (current_time - last_capture_time).total_seconds()
+                    if time_diff >= 3:
+                        # Chụp ảnh và lưu vào thư mục
+                        last_capture_time = current_time
+                        file_name = f"{output_dir}/motion_{current_time.strftime('%Y%m%d%H%M%S')}.jpg"
+                        cv2.imwrite(file_name, frame)
+
+                cv2.imshow("Motion Detection", frame)
+
+                if cv2.waitKey(40) == 27:  # Nhấn Esc để thoát
+                    break
+        cap.release()
+        cv2.destroyAllWindows()
+def start_camera():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+    ret, frame = cap.read()
     frame = cv2.resize(frame, (640, 480))
-
-    # Lật lại cả khung hình
-    frame = cv2.flip(frame, 1)
-
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (25, 25), 0)
-
-    abs_img = cv2.absdiff(last_frame[y:y + h, x:x + w], gray[y:y + h, x:x + w])
     last_frame = gray
+    frame = cv2.flip(frame, 1)
+    # Chọn vùng cần quan sát
+    roi = cv2.selectROI('Video', frame, fromCenter=False, showCrosshair=True)
+    cv2.destroyWindow('Video')
+    x, y, w, h = int(roi[0]), int(roi[1]), int(roi[2]), int(roi[3])
 
-    _, img_mask = cv2.threshold(abs_img, 30, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print(x, y, w, h)
 
-    for contour in contours:
-        if cv2.contourArea(contour) < 900:
-            continue
+    # Tạo thư mục chứa ảnh được chụp
+    output_dir = "captured_images"
+    os.makedirs(output_dir, exist_ok=True)
 
-        x1, y1, w1, h1 = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x1 + x, y1 + y), (x1 + x + w1, y1 + y + h1), (0, 255, 0), 2)
-        cv2.putText(frame, 'Canh Bao', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    # Biến thời gian để theo dõi thời gian đã trôi qua từ lần chụp trước đó
+    last_capture_time = datetime.now()
 
-        # Kiểm tra thời gian từ lần chụp trước đó
-        current_time = datetime.now()
-        time_diff = (current_time - last_capture_time).total_seconds()
-        if time_diff >= 3:
-            # Chụp ảnh và lưu vào thư mục
-            last_capture_time = current_time
-            file_name = f"{output_dir}/motion_{current_time.strftime('%Y%m%d%H%M%S')}.jpg"
-            cv2.imwrite(file_name, frame)
+    while True:
+        _, frame = cap.read()
+        print(ret)
+        print(frame.shape)
+        frame = cv2.resize(frame, (640, 480))
 
-    cv2.imshow("Motion Detection", frame)
+        # Lật lại cả khung hình
+        frame = cv2.flip(frame, 1)
+        # Lấy thời gian hiện tại
+        current_time = datetime.now().strftime("%H:%M:%S")
+        current_date = datetime.now().strftime("%d/%m/%Y")
 
-    if cv2.waitKey(40) == 27:  # Nhấn Esc để thoát
-        break
+        # Hiển thị thời gian lên video
+        cv2.putText(frame,current_time, (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(frame,current_date, (15, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (25, 25), 0)
+
+        abs_img = cv2.absdiff(last_frame[y:y + h, x:x + w], gray[y:y + h, x:x + w])
+        last_frame = gray
+
+        _, img_mask = cv2.threshold(abs_img, 10, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(img_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            if cv2.contourArea(contour) < 900:
+                continue
+
+            x1, y1, w1, h1 = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x1 + x, y1 + y), (x1 + x + w1, y1 + y + h1), (0, 255, 0), 2)
+            cv2.putText(frame, 'Canh Bao', (15, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+            # Kiểm tra thời gian từ lần chụp trước đó
+            current_time = datetime.now()
+            time_diff = (current_time - last_capture_time).total_seconds()
+            if time_diff >= 3:
+                # Chụp ảnh và lưu vào thư mục
+                last_capture_time = current_time
+                file_name = f"{output_dir}/motion_{current_time.strftime('%Y%m%d%H%M%S')}.jpg"
+                cv2.imwrite(file_name, frame)
+
+        cv2.imshow("Motion Detection", frame)
+
+        if cv2.waitKey(40) == 27:  # Nhấn Esc để thoát
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+root = tk.Tk()
+root.title("Motion Detection")
+
+file_button = tk.Button(root, text="Select Video File", command=select_file)
+file_button.pack()
+
+camera_button = tk.Button(root, text="Start Camera", command=start_camera)
+camera_button.pack()
+
+root.mainloop()
